3cea0ce28994bedd3a87f696fdcb2e4f04686f3b
NotSault <rosiquevictor@gmail.com>
2023-08-30 10:50:27 +0200

    update

--- app.js
+++ app.js
@@ -1,66 +1,64 @@
 const express = require('express');
-const fs = require('fs');
 const crypto = require('crypto-js');
+const Redis = require('ioredis');
+const dotenv = require('dotenv');
 const app = express();
 const port = 443;
 
-// Réinitialiser le fichier json.db à chaque redémarrage du serveur
-fs.writeFileSync('json.db', JSON.stringify({}));
+// ======== Load Environment Variables ========
+dotenv.config({ path: './redis.env' });
+const { MASTER_KEY, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
+
+// ======== Configuration Redis ========
+const client = new Redis({
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD
+});
+
+client.on('connect', () => {
+    console.log('Connecté à Redis');
+});
+client.on('error', err => {
+    console.error('Erreur Redis:', err);
+});
+
 app.use(express.json());
 app.use(express.static('public'));
-
-
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
-
-
-const dbFile = 'json.db';
-let db = {};
-
-if (fs.existsSync(dbFile)) {
-  const rawData = fs.readFileSync(dbFile);
-  db = JSON.parse(rawData);
-}
-
-const saveDb = () => {
-  const data = JSON.stringify(db);
-  fs.writeFileSync(dbFile, data);
-};
 
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/upload', (req, res) => {
-    const { text } = req.body;
-    const encryptedText = crypto.AES.encrypt(text, 'secret-key').toString();
-    const id = new Date().getTime().toString();
-    
-    db[id] = encryptedText;
-    saveDb();
-    
-    const shareLink = `https://note-m.onrender.com/download/${id}`;
-    
-    res.render('share', { link: shareLink });
-  });
+app.post('/upload', async (req, res) => {
+  const { text } = req.body;
+  const encryptedText = crypto.AES.encrypt(text, MASTER_KEY).toString();
+  const id = new Date().getTime().toString();
+  
+  // Sauvegarder dans Redis
+  await client.set(id, encryptedText, 'EX', 600); // Expire après 600 secondes
+  
+  const shareLink = `https://note-m.onrender.com/download/${id}`;
+  res.render('share', { link: shareLink });
+});
 
-app.get('/download/:id', (req, res) => {
-   const { id } = req.params;
-   const db = JSON.parse(fs.readFileSync('json.db'));
-   const encryptedText = db[id];
-    
-   if (!encryptedText) {
-     return res.status(404).send('Text not found');
-   }
-    
-   const decryptedText = crypto.AES.decrypt(encryptedText, 'secret-key').toString(crypto.enc.Utf8);
-    
-   // Supprimer immédiatement l'entrée de la base de données
-  delete db[id];
-   fs.writeFileSync('json.db', JSON.stringify(db));
-    
-  res.render('download', { text: decryptedText }); 
-  });
+app.get('/download/:id', async (req, res) => {
+  const { id } = req.params;
+  const encryptedText = await client.get(id);
+  
+  if (!encryptedText) {
+    return res.status(404).send('Text not found');
+  }
+  
+  const decryptedText = crypto.AES.decrypt(encryptedText, MASTER_KEY).toString(crypto.enc.Utf8);
+  
+  // Supprimer immédiatement l'entrée de Redis
+  await client.del(id);
+  
+  res.render('download', { text: decryptedText });
+});
 
 app.listen(port, () => {
   console.log(`Server running at https://note-m.onrender.com:${port}/`);
