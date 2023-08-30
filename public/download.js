8b7c5e59bc3302aff1e50c2421caf889fdd7b54e
NotSault <rosiquevictor@gmail.com>
2023-08-30 19:24:28 +0200

    test

--- public/download.js
+++ public/download.js
@@ -1,35 +1,34 @@
-  // Function to confirm reading
-  function confirmRead() {
-    const secureTextElement = document.getElementById("secureText");
-    secureTextElement.innerText = '<%= text %>';
-      markAsRead();
-      closeModal();
-   }
+function confirmRead() {
+  const secureTextElement = document.getElementById("secureText");
+  const text = secureTextElement.getAttribute('data-text'); // Utilisation d'un attribut de données
+  secureTextElement.innerText = text;
+  markAsRead();
+  closeModal();
+}
 
-   // Function to close the modal without reading
-   function closeModal() {
-     document.getElementById("myModal").style.display = "none";
-   }
+function closeModal() {
+  document.getElementById("myModal").style.display = "none";
+}
 
-   function markAsRead() {
-     const id = '<%= id %>';
-     fetch(`/markAsRead/${id}`, {
-       method: 'POST'
-     }).then(response => {
-       if (response.ok) {
-         console.log('Note marked as read and deleted');
-       } else {
-         console.log('Failed to mark note as read');
-       }
-     });
-   }
+function markAsRead() {
+  const id = document.getElementById("secureText").getAttribute('data-id'); // Utilisation d'un attribut de données
+  fetch(`/markAsRead/${id}`, {
+    method: 'POST'
+  }).then(response => {
+    if (response.ok) {
+      console.log('Note marked as read and deleted');
+    } else {
+      console.log('Failed to mark note as read');
+    }
+  });
+}
 
-   function copyToClipboard() {
-     const textArea = document.createElement('textarea');
-     textArea.value = document.getElementById("secureText").innerText;
-     document.body.appendChild(textArea);
-     textArea.select();
-     document.execCommand('copy');
-     document.body.removeChild(textArea);
-     alert('Note copié dans le presse-papiers');
-   }
+function copyToClipboard() {
+  const textArea = document.createElement('textarea');
+  textArea.value = document.getElementById("secureText").innerText;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand('copy');
+  document.body.removeChild(textArea);
+  alert('Note copié dans le presse-papiers');
+}
