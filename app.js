adc67e5b6b62a8250f82e2d7c420c14767dd186c
NotSault <rosiquevictor@gmail.com>
2023-09-04 10:18:37 +0200

    code for note fix

--- app.js
+++ app.js
@@ -70,8 +70,10 @@ app.get('/note/:id', async (req, res) => {
   }
 
   let promptScript = "";
+  let decryptedAccessCode = "";
 
   if (encryptedData.accessCode) {
+    decryptedAccessCode = crypto.AES.decrypt(encryptedData.accessCode, MASTER_KEY).toString(crypto.enc.Utf8);
     promptScript = `
       <script>
         const userAccessCode = prompt("Entrez le code d'accès pour cette note:");
@@ -84,8 +86,12 @@ app.get('/note/:id', async (req, res) => {
   }
   
   const decryptedText = crypto.AES.decrypt(encryptedData.text, MASTER_KEY).toString(crypto.enc.Utf8);
-  res.render('note', { text: decryptedText, id: id, requiresAccessCode: !!encryptedData.accessCode });
-  res.render('note', { text: decryptedText, id: id, promptScript: promptScript });
+  
+  res.render('note', { 
+    text: decryptedText, 
+    id: id, 
+    promptScript: promptScript 
+  });
 });
 
 // Route pour marquer une note comme lue et la supprimer
