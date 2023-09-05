7f1d4a02a394df794e96e283792ad81e0b185920
NotSault <rosiquevictor@gmail.com>
2023-09-05 12:52:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -5,103 +5,86 @@ const dotenv = require('dotenv');
 const app = express();
 const port = 3000;
 
-// Charger les variables d'environnement
+// Charger les variables d'environnement depuis le fichier .env
 dotenv.config({ path: './process.env' });
 const { MASTER_KEY, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 
 // Configurer la connexion à Redis
 const client = new Redis({
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-  password: REDIS_PASSWORD
+    host: REDIS_HOST,
+    port: REDIS_PORT,
+    password: REDIS_PASSWORD
 });
 
-client.on('connect', async () => {
-  console.log('Connecté à Redis');
-  if (!(await client.exists('noteCount'))) {
-    await client.set('noteCount', 0);
-  }
+// Événements de connexion et d'erreur pour Redis
+client.on('connect', () => {
+    console.log('Connecté à Redis');
 });
-
 client.on('error', err => {
-  console.error('Erreur Redis:', err);
+    console.error('Erreur Redis:', err);
 });
 
+// Middleware pour le parsing du JSON et des formulaires
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
+
+// Configurer le moteur de vue EJS
 app.set('view engine', 'ejs');
 
+// Route pour la page d'accueil
 app.get('/', (req, res) => {
   res.render('index');
 });
 
+// Route pour l'upload de texte
 app.post('/upload', async (req, res) => {
   const { text } = req.body;
   const encryptedText = crypto.AES.encrypt(text, MASTER_KEY).toString();
   const id = new Date().getTime().toString();
   
-  await client.set(id, encryptedText, 'EX', 600);
-  await client.incr('noteCount');
-
-  const dashboardData = {
-    created_at: new Date(),
-    action: "créée"
-  };
-  await client.lpush('dashboardData', JSON.stringify(dashboardData));
-
+  // Sauvegarder le texte chiffré dans Redis
+  await client.set(id, encryptedText, 'EX', 600); // Expire après 600 secondes
+  
   const shareLink = `https://note-m.cyclic.app/note/${id}`;
   res.render('share', { link: shareLink });
 });
 
+// Route pour télécharger une note
 app.get('/note/:id', async (req, res) => {
   const { id } = req.params;
   const encryptedText = await client.get(id);
   
   if (!encryptedText) {
-    return res.status(404).render('404');
+    return res.status(404).render('404'); // Page d'erreur 404
   }
   
-  const dashboardData = {
-    accessed_at: new Date(),
-    action: "consultée"
-  };
-  await client.lpush('dashboardData', JSON.stringify(dashboardData));
-
   const decryptedText = crypto.AES.decrypt(encryptedText, MASTER_KEY).toString(crypto.enc.Utf8);
+  
   res.render('note', { text: decryptedText, id: id });
 });
 
+// Route pour marquer une note comme lue et la supprimer
 app.post('/markAsRead/:id', async (req, res) => {
   const { id } = req.params;
   const encryptedText = await client.get(id);
   
   if (!encryptedText) {
-    return res.status(404).render('404');
+    return res.status(404).render('404'); // Page d'erreur 404
   }
   
+  // Supprimer la note de Redis
   await client.del(id);
-  await client.decr('noteCount');
-
-  const dashboardData = {
-    deleted_at: new Date(),
-    action: "supprimée"
-  };
-  await client.lpush('dashboardData', JSON.stringify(dashboardData));
-
-  res.status(200).send('Note marquée comme lue et supprimée');
+  
+  res.status(200).send('Note marked as read and deleted');
 });
 
-app.get('/dashboard', async (req, res) => {
-  const rawData = await client.lrange('dashboardData', 0, -1);
-  const dashboardData = rawData.map(data => JSON.parse(data));
-  res.json(dashboardData);
-});
-
+// Middleware pour gérer les erreurs 404
 app.use((req, res, next) => {
   res.status(404).render('404');
 });
 
+// Démarrer le serveur
 app.listen(port, () => {
-  console.log(`Le serveur fonctionne sur https://note-m.cyclic.app:${port}/`);
+  console.log(`Server running at https://note-m.cyclic.app:${port}/`);
 });
