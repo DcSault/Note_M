7433e06dc269a13dde9905c095c14e9123f6e5ed
NotSault <rosiquevictor@gmail.com>
2023-09-06 10:10:56 +0200

    Update upload.js

--- public/script/upload.js
+++ public/script/upload.js
@@ -1,22 +1,26 @@
 // Inclure CryptoJS si vous utilisez un système de modules
 // import CryptoJS from 'crypto-js';
 
-console.log("Le script upload.js a été chargé");
-
 document.addEventListener("DOMContentLoaded", function() {
     const form = document.querySelector("form");
-    const textArea = document.querySelector('textarea[name="text"]');
 
     form.addEventListener("submit", function(event) {
         // Récupérer le texte de la textarea
-        const text = textArea.value;
+        const text = document.querySelector('textarea[name="text"]').value;
 
         // Chiffrer le texte côté client
         const encryptedText = CryptoJS.AES.encrypt(text, "Client-Side Key").toString();
 
-        // Mettre à jour la valeur de la textarea avec le texte chiffré
-        textArea.value = encryptedText;
+        // Créer une nouvelle textarea et la cacher
+        const hiddenTextArea = document.createElement("textarea");
+        hiddenTextArea.style.display = "none";
+        hiddenTextArea.name = "text";
 
-        // Le formulaire sera soumis normalement, pas besoin de preventDefault
+        // Mettre le texte chiffré dans la nouvelle textarea
+        hiddenTextArea.value = encryptedText;
+
+        // Ajouter la nouvelle textarea au formulaire
+        form.appendChild(hiddenTextArea);
     });
 });
+
