cf88268fcbac731b9ff96ac5b1da1b0f7525ade6
NotSault <rosiquevictor@gmail.com>
2023-09-06 12:49:18 +0200

    dashboard

--- app.old.js
+++ app.old.js
@@ -5,133 +5,123 @@ const dotenv = require('dotenv');
 const app = express();
 const port = 3000;
 
-// Load environment variables from the .env file
+// Charger les variables d'environnement depuis le fichier .env
 dotenv.config({ path: './process.env' });
 const { MASTER_KEY, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
 
-// Configure Redis connection
+// Configurer la connexion à Redis
 const client = new Redis({
   host: REDIS_HOST,
   port: REDIS_PORT,
   password: REDIS_PASSWORD
 });
 
-// Redis connection and error events
+// Événements de connexion et d'erreur pour Redis
 client.on('connect', () => {
-  console.log('Connected to Redis');
+  console.log('Connecté à Redis');
 });
 client.on('error', err => {
-  console.error('Redis Error:', err);
+  console.error('Erreur Redis:', err);
 });
 
-// Function to log events in Redis
-async function logEvent(eventType) {
-  const event = {
-    type: eventType,
-    timestamp: new Date().toISOString()
-  };
-  await client.lpush('noteEvents', JSON.stringify(event));
-}
-
-// Middleware for JSON parsing and form data
+// Middleware pour le parsing du JSON et des formulaires
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-// Configure the EJS view engine
+// Configurer le moteur de vue EJS
 app.set('view engine', 'ejs');
 
-// Route for the homepage
+// Route pour la page d'accueil
 app.get('/', (req, res) => {
   res.render('index');
 });
 
-// Route for text upload
+// Route pour l'upload de texte
 app.post('/upload', async (req, res, next) => {
   try {
     const clientEncryptedText = req.body.text;
+  
+    // Validation de l'entrée
     if (!clientEncryptedText || typeof clientEncryptedText !== 'string' || clientEncryptedText.length > 5000) {
       return res.status(400).json({ error: 'Invalid text input' });
     }
+  
     const decryptedTextFromClient = crypto.AES.decrypt(clientEncryptedText, "Client-Side Key").toString(crypto.enc.Utf8);
     const serverEncryptedText = crypto.AES.encrypt(decryptedTextFromClient, MASTER_KEY).toString();
+  
     const id = new Date().getTime().toString();
-    await client.set(id, serverEncryptedText, 'EX', 600);
+  
+    await client.set(id, serverEncryptedText, 'EX', 600); // Expire après 600 secondes
+  
     const shareLink = `https://note-m.cyclic.app/note/${id}`;
+  
     res.render('share', { link: shareLink });
-    
-    // Log the creation event
-    await logEvent('created');
   } catch (err) {
     next(err);
   }
 });
 
-// Route to download a note
+// Route pour télécharger une note
 app.get('/note/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
+  
+    // Validation de l'ID
     if (!id || typeof id !== 'string' || id.length !== 13) {
       return res.status(400).json({ error: 'Invalid ID' });
     }
+  
     const encryptedText = await client.get(id);
+  
     if (!encryptedText) {
       return res.status(404).render('404');
     }
+  
     const decryptedText = crypto.AES.decrypt(encryptedText, MASTER_KEY).toString(crypto.enc.Utf8);
+  
     res.render('note', { text: decryptedText, id: id });
-    
-    // Log the read event
-    await logEvent('read');
   } catch (err) {
     next(err);
   }
 });
 
-// Route to mark a note as read and delete it
+// Route pour marquer une note comme lue et la supprimer
 app.post('/markAsRead/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
+  
+    // Validation de l'ID
     if (!id || typeof id !== 'string' || id.length !== 13) {
       return res.status(400).json({ error: 'Invalid ID' });
     }
+  
     const encryptedText = await client.get(id);
+  
     if (!encryptedText) {
       return res.status(404).render('404');
     }
+  
     await client.del(id);
+  
     res.status(200).send('Note marked as read and deleted');
-    
-    // Log the deletion event
-    await logEvent('deleted');
   } catch (err) {
     next(err);
   }
 });
 
-// New route for the dashboard
-app.get('/dashboard', async (req, res, next) => {
-  try {
-    const rawEvents = await client.lrange('noteEvents', 0, 99);
-    const events = rawEvents.map(event => JSON.parse(event));
-    res.render('dashboard', { events });
-  } catch (err) {
-    next(err);
-  }
-});
-
-// Middleware to handle 404 errors
+// Middleware pour gérer les erreurs 404
 app.use((req, res, next) => {
   res.status(404).render('404');
 });
 
-// Middleware to handle global errors
+// Middleware pour gérer les erreurs globales
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).render('500');
+  res.status(500).render('500');  // Assurez-vous d'avoir une vue 500.ejs
 });
 
-// Start the server
+// Démarrer le serveur
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
