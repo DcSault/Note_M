fa080ade348ae8d40fa808c72218f92f8356f9f5
NotSault <rosiquevictor@gmail.com>
2023-09-07 18:13:37 +0200

    Events

--- public/script/dashboard.js
+++ public/script/dashboard.js
@@ -1,46 +1,60 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Utilisation de la variable globale définie dans le fichier EJS
-    const events = eventsData;
-
-    // Vérifie que la variable events existe et est un tableau
-    if (Array.isArray(events)) {
-        // Prépare les données pour le graphique
-        const eventTypes = ['created', 'read', 'deleted'];
-        const eventData = eventTypes.map(type => {
-            return events.filter(event => event.type === type).length;
-        });
-
-        // Crée le graphique
-        const ctx = document.getElementById('eventChart').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: eventTypes,
-                datasets: [{
-                    label: '# of Events',
-                    data: eventData,
-                    backgroundColor: [
-                        'rgba(75, 192, 192, 0.5)',  // created: blue
-                        'rgba(255, 206, 86, 0.5)',  // read: yellow
-                        'rgba(255, 99, 132, 0.5)'   // deleted: red
-                    ],
-                    borderColor: [
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(255, 99, 132, 1)'
-                    ],
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    } else {
-        console.error("La variable 'events' n'est pas un tableau ou n'est pas définie.");
-    }
-});
+document.addEventListener('DOMContentLoaded', function() {
+    const events = [
+      { type: 'created', timestamp: '2023-01-01' },
+      { type: 'read', timestamp: '2023-01-02' },
+      // Ajoutez plus d'événements ici...
+    ];
+  
+    const eventTypes = ['created', 'read', 'deleted'];
+    const eventData = eventTypes.map(type => {
+      return events.filter(event => event.type === type).length;
+    });
+  
+    // Bar Chart
+    const ctxBar = document.getElementById('eventChartBar').getContext('2d');
+    const barChart = new Chart(ctxBar, {
+      type: 'bar',
+      data: {
+        labels: eventTypes,
+        datasets: [{
+          label: '# of Events',
+          data: eventData,
+          backgroundColor: [
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(255, 99, 132, 0.5)'
+          ],
+          borderColor: [
+            'rgba(75, 192, 192, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(255, 99, 132, 1)'
+          ],
+          borderWidth: 1
+        }]
+      }
+    });
+  
+    // Pie Chart
+    const ctxPie = document.getElementById('eventChartPie').getContext('2d');
+    const pieChart = new Chart(ctxPie, {
+      type: 'pie',
+      data: {
+        labels: eventTypes,
+        datasets: [{
+          data: eventData,
+          backgroundColor: [
+            'rgba(75, 192, 192, 0.5)',
+            'rgba(255, 206, 86, 0.5)',
+            'rgba(255, 99, 132, 0.5)'
+          ],
+          borderColor: [
+            'rgba(75, 192, 192, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(255, 99, 132, 1)'
+          ],
+          borderWidth: 1
+        }]
+      }
+    });
+  });
+  
