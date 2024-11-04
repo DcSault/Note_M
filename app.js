a8e3dd5af90fc8ac6ac2fa742b1eb61dd6effda7
NotSault <rosiquevictor@gmail.com>
2024-11-04 11:43:08 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,17 +6,24 @@ const app = express();
 const port = 3000;
 
 // Load environment variables from the .env file
-dotenv.config({ path: './process.env' });
-const { MASTER_KEY, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD } = process.env;
+dotenv.config({ path: './.env' });
+const { MASTER_KEY, REDIS_HOST, REDIS_PORT, REDIS_PASSWORD, BASE_URL } = process.env;
+
+// Vérification des variables d'environnement chargées
+console.log("REDIS_HOST:", REDIS_HOST);
+console.log("REDIS_PORT:", REDIS_PORT);
+console.log("BASE_URL:", BASE_URL);
 
 // Configure Redis connection
 const client = new Redis({
-  host: REDIS_HOST,
-  port: REDIS_PORT,
-  password: REDIS_PASSWORD
+  host: REDIS_HOST || '10.20.30.140',
+  port: Number(REDIS_PORT) || 2404,
+  password: REDIS_PASSWORD || undefined,
+  retryStrategy(times) {
+    return Math.min(times * 50, 2000);
+  }
 });
 
-// Redis connection and error events
 client.on('connect', () => {
   console.log('Connected to Redis');
 });
@@ -24,7 +31,6 @@ client.on('error', err => {
   console.error('Redis Error:', err);
 });
 
-// Function to log events in Redis
 async function logEvent(eventType) {
   const event = {
     type: eventType,
@@ -33,15 +39,12 @@ async function logEvent(eventType) {
   await client.lpush('noteEvents', JSON.stringify(event));
 }
 
-// Middleware for JSON parsing and form data
 app.use(express.json());
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 
-// Configure the EJS view engine
 app.set('view engine', 'ejs');
 
-// Route for the homepage
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -56,8 +59,11 @@ app.post('/upload', async (req, res, next) => {
     const decryptedTextFromClient = crypto.AES.decrypt(clientEncryptedText, "Client-Side Key").toString(crypto.enc.Utf8);
     const serverEncryptedText = crypto.AES.encrypt(decryptedTextFromClient, MASTER_KEY).toString();
     const id = new Date().getTime().toString();
-    await client.set(id, serverEncryptedText, 'EX', 600);
-    const shareLink = `https://note-m.cyclic.app/note/${id}`;
+
+    // Configure expiration to 1 day (86400 seconds)
+    await client.set(id, serverEncryptedText, 'EX', 86400);
+
+    const shareLink = `${BASE_URL}/note/${id}`;
     res.render('share', { link: shareLink });
     
     // Log the creation event
@@ -67,7 +73,6 @@ app.post('/upload', async (req, res, next) => {
   }
 });
 
-// Route to download a note
 app.get('/note/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
@@ -81,14 +86,12 @@ app.get('/note/:id', async (req, res, next) => {
     const decryptedText = crypto.AES.decrypt(encryptedText, MASTER_KEY).toString(crypto.enc.Utf8);
     res.render('note', { text: decryptedText, id: id });
     
-    // Log the read event
     await logEvent('read');
   } catch (err) {
     next(err);
   }
 });
 
-// Route to mark a note as read and delete it
 app.post('/markAsRead/:id', async (req, res, next) => {
   try {
     const { id } = req.params;
@@ -102,25 +105,21 @@ app.post('/markAsRead/:id', async (req, res, next) => {
     await client.del(id);
     res.status(200).send('Note marked as read and deleted');
     
-    // Log the deletion event
     await logEvent('deleted');
   } catch (err) {
     next(err);
   }
 });
 
-// Middleware to handle 404 errors
 app.use((req, res, next) => {
   res.status(404).render('404');
 });
 
-// Middleware to handle global errors
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).render('500');
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
